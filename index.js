49b0fc3e9c3f2ca69208f63c017612f83ad75a64
solstice23 <solstice233@gmail.com>
2024-03-01 15:40:18 -0500

    init

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import NodeCache from 'node-cache';
+
+const app = express();
+
+const history = [];
+
+app.get('/history', async function (req, res) {
+	let token = req.query.token;
+	if (token !== 'svkj7bdikg') {
+		res.status(403).send('Wrong token');
+		return;
+	}
+	const str = history.map(({time, content}) => {
+		const formattedTime = new Date(time).toISOString();
+		const ago = (Date.now() - time) / 1000;
+		return `
+		<div>Time: ${formattedTime}</div>
+		<div>${ago} seconds ago</div>
+		<div>Content: <br><pre>${content}</pre></div>
+		`;
+	});
+	res.send(str.join(''));
+});
+
+app.get('/record', async function (req, res) {
+	const content = req.query.c;
+	history.push({time: Date.now(), content});
+	res.send('Recorded');
+});
+
+app.listen(process.env.PORT || 3000);
