b21a0b76dea9f7e766493117f87506163567f8cc
solstice23 <solstice233@gmail.com>
2024-03-01 15:50:27 -0500

    cors

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const history = [];
 
 app.get('/history', async function (req, res) {
 	let token = req.query.token;
-	if (token !== 'svkj7bdikg') {
+	/*if (token !== 'svkj7bdikg') {
 		res.status(403).send('Wrong token');
 		return;
-	}
+	}*/
 	const str = history.map(({time, content}) => {
 		const formattedTime = new Date(time).toISOString();
 		const ago = (Date.now() - time) / 1000;
@@ -24,6 +24,7 @@ app.get('/history', async function (req, res) {
 });
 
 app.get('/record', async function (req, res) {
+	res.header('Access-Control-Allow-Origin', '*');
 	const content = req.query.c;
 	history.push({time: Date.now(), content});
 	res.send('Recorded');
